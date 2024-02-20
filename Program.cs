18d7c880285e68cb7cc13dd6a5fdcc5871618e72
Usuario <Usuario@CPD02-M18>
2024-02-20 13:44:09 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_teste1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("olá Mundo");
+            //string
+            Console.WriteLine("lógica");
+            //Número decimal
+            Console.WriteLine(1.5);
+            //Número inteiro
+            Console.WriteLine(2);
+            //Cáuculo
+            Console.WriteLine(2 + 2);
+            //teste de concatenação
+            Console.WriteLine("O resultado é: " + (2 + 2));
+            Console.WriteLine( 2 + 2 + " O resultado é");
+            Console.WriteLine(2 +( 2 * 3));
+            //Dá um Bip
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep(); Console.Beep();
+            Console.ReadKey();
+        }
+    }
+}
